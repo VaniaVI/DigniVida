e3161750ce2a6a6f199010f9d9acfddb9a03cbe8
444ldea <felipe.aldea@genstudents.org>
2025-06-04 14:25:03 -0400

    ruta login verificacion

--- dignivida-react/src/routes.jsx
+++ dignivida-react/src/routes.jsx
@@ -3,6 +3,8 @@ import Home from './pages/Home/Home';
 import PerfilBeneficiario from './pages/PerfilBeneficiario/PerfilBeneficiario';
 import BeneficiarioSeguimiento from './pages/BeneficiarioSeguimiento/BeneficiarioSeguimiento';
 import Layout from './pages/Layout';
+import Login from './pages/Login';
+import VerificacionSms from './pages/VerificacionSms'
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -10,6 +12,8 @@ export const router = createBrowserRouter(
             <Route path="/" element={<Home />}  />
             <Route path="/perfilBeneficiario" element={<PerfilBeneficiario />} />
             <Route path="/beneficiarioSeguimiento" element={<BeneficiarioSeguimiento />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/verificacionsms" element={<VerificacionSms/>} />
         </Route>
     )
 )
