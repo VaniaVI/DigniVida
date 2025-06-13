e5b0f193e8999de048f95a1d93d8dc857a3c9683
VaniaVI <vania.vargas.igor@gmail.com>
2025-06-13 13:29:59 -0400

    Se agrega controlador, rutas, y se meejora el servidor para beneficiario y usuario. ademas de instalar dependencias

---
+++ dignivida-backend/rutas/voluntarios.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const voluntarioController = require('../controladores/voluntarioController');
+
+// Rutas CRUD
+router.get('/', voluntarioController.getVoluntarios);
+router.post('/', voluntarioController.createVoluntario);
+router.put('/:id', voluntarioController.updateVoluntario);
+router.delete('/:id', voluntarioController.deleteVoluntario);
+
+module.exports = router;
