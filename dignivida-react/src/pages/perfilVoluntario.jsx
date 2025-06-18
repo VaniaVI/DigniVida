1be920d102f437a494ce44f18050fef0245a3a32
MightySoru <fabian.serrano.vera@gmail.com>
2025-06-18 05:04:06 -0400

    perfilVoluntariocss

--- dignivida-react/src/pages/perfilVoluntario.jsx
+++ dignivida-react/src/pages/perfilVoluntario.jsx
@@ -1,5 +1,7 @@
+
+
 import React, { useState, useEffect } from 'react';
-import './EstilosBeneficiarioReact.css'; 
+import './perfilVoluntario.css'; 
 
 const PerfilVoluntario = () => {
   const [perfil, setPerfil] = useState({
